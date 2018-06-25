b4e0541599bed6b09928d979d1bde0cc1633d0e4
S. Narazaki <narazakilaboratory@gmail.com>
2018-06-26 08:08:38 +0900

    ditto

--- src/Lib.hs
+++ src/Lib.hs
@@ -75,7 +75,8 @@ homePage = H.docTypeHtml $ do
         H.form ! A.method "POST" ! A.action "run" $ do
         H.p $ H.textarea ! A.name "program" ! A.cols "50" ! A.rows "10" $ "MOV R1, R2"
         H.p $ H.button ! A.type_ "submit" ! A.name "action" ! A.value "send" $ "RUN"
-      H.pre ! A.style "background: #ccf;" $ H.toMarkup (repl "MOV R1, R2\n")
+      H.h1 $ "Rusult and Disassembled Code"
+      H.pre $ H.code ! A.style "font-family: Courier, monospace; background: #eef;" $ H.toMarkup (repl "MOV R1, R2\n")
 
 resultPage :: Code -> H.Html
 resultPage (Code str) = H.docTypeHtml $ do
@@ -88,7 +89,7 @@ resultPage (Code str) = H.docTypeHtml $ do
         H.p $ H.textarea ! A.name "program" ! A.cols "50" ! A.rows "10" $ H.toMarkup str
         H.p $ H.button ! A.type_ "submit" ! A.name "action" ! A.value "send" $ "RERUN"
       H.h1 $ "Rusult and Disassembled Code"
-      H.pre ! A.style "font-family: monospace; background: #eef;" $ H.toMarkup (repl (str ++ "\n"))
+      H.pre $ H.code ! A.style "font-family: Courier, monospace; background: #eef;" $ H.toMarkup (repl (str ++ "\n"))
 
 repl :: String -> String
 repl str' = l1 ++ "\n" ++ concatMap toBit (lines str)
