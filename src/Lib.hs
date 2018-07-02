9f4c7373cb7fe717039a678d5781dc930bdda90f
Shuji Narazaki <shuji.narazaki@gmail.com>
2018-07-02 15:08:58 +0000

    Randomize ui

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,6 +8,7 @@ module Lib
     ) where
 
 import Control.Monad
+import Control.Monad.IO.Class
 import Data.Aeson
 import Data.Aeson.TH
 import GHC.Generics
@@ -18,6 +19,8 @@ import Servant.HTML.Blaze
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
 import Text.Blaze.Html5 ((!))
+import System.Random
+import System.IO.Unsafe
 import System.ReadEnvVar (readEnvDef)
 import qualified PDP11 as PDP
 import qualified Assembler as PA
@@ -25,11 +28,12 @@ import qualified Simulator as PS
 import Web.FormUrlEncoded(FromForm(..), ToForm(..))
 
 version :: String
-version = "0.4.1.0"
+version = "0.5.0.0"
 
 data Code = Code
  {
-   program :: String
+   program   :: String
+ , randomize :: String
  } deriving (Eq, Show, Generic)
 
 instance FromForm Code
@@ -57,18 +61,20 @@ homePage = H.docTypeHtml $ do
   H.head $ do
     H.title . H.toHtml $ "PDP11 simulator (version " ++ version ++ ")"
     H.body $ do
-      H.h1 "Welcome to a PDP11 simulator."
+      H.h1 "Welcome to a PDP11 simulator"
       H.p "Fill you code in"
       H.p $
         H.form ! A.method "POST" ! A.action "run" $ do
         H.p $ H.textarea ! A.name "program" ! A.cols "40" ! A.rows "10" $ "MOV R1, R2"
-        H.p $ H.button ! A.type_ "submit" ! A.name "action" ! A.value "send" $ "RUN"
+        H.p $ do
+          H.button ! A.type_ "submit" ! A.name "action" ! A.value "send" $ "RUN"
+          H.input ! A.type_ "hidden" ! A.name "randomize" ! A.value "RandomizeOff"
       H.hr
       H.p ! A.style "text-align: right" $ "powered by Servant, a type-safe web server written in Haskell."
       H.p ! A.style "text-align: right;" $ H.toMarkup ("version " ++ version ++ " by nrzk, nagasaki-u.")
 
 resultPage :: Code -> H.Html
-resultPage (Code str) = H.docTypeHtml $ do
+resultPage (Code str rnd) = H.docTypeHtml $ do
   H.head $ do
     H.style ! A.type_ "text/css" $ "<!-- \n\
 \table,tr,td,th {border:1px black solid;border-collapse:collapse;font-family:monospace;}\n\
@@ -80,10 +86,15 @@ resultPage (Code str) = H.docTypeHtml $ do
     H.title "A PDP11 simulator"
     H.body $ do
       H.h1 "Assembly Code"
-      H.p $
+      H.p $ do
         H.form ! A.method "POST" ! A.action "run" $ do
-        H.p $ H.textarea ! A.name "program" ! A.cols "40" ! A.rows "10" $ H.toMarkup str
-        H.p $ H.button ! A.type_ "submit" ! A.name "action" ! A.value "send" $ "UPDATE"
+          H.p $ H.textarea ! A.name "program" ! A.cols "40" ! A.rows "10" $ H.toMarkup str
+          H.p $ do
+            H.select ! A.id "randomize" ! A.name "randomize" $ do
+              H.optgroup ! A.label "Memory Randomization" $ do
+                H.option "randomize on"
+                H.option ! A.selected "randomize off" $ "randomize off"
+            H.button ! A.type_ "submit" ! A.name "action" ! A.value "send" ! A.style "margin-left: 120px;" $ "UPDATE"
       let prg = shaping str
       if null prg
         then do
@@ -96,7 +107,8 @@ resultPage (Code str) = H.docTypeHtml $ do
               H.h1 $ H.toMarkup $ "Binary Representation (ver. " ++ PDP.version ++ ")"
               H.pre ! A.style "width:300px;background:#f8f8f8;border:1px solid #777;margin:8px;padding:8px;" $ H.code $ H.toMarkup l
           H.h1 $ H.toMarkup $ "Execution Trace (ver. " ++ PS.version ++ ")"
-          case repl prg of
+          let mac = unsafePerformIO $ makeMachine (rnd == "randomize on")
+          case repl mac prg of
             Left str -> H.pre ! A.style "background: #fee;" $ H.toMarkup str
             Right lst -> do
               H.table $ do
@@ -124,8 +136,16 @@ shaping :: String -> String
 shaping str = unlines . filter (not . null) . map trim . lines $ str
   where trim = reverse . dropWhile (`elem` [' ', '\t', '\r']) . reverse . dropWhile (`elem` [' ', '\t'])
 
-repl :: String -> Either String [([Int], [Int], [Int], Int, PDP.ASM)]
-repl str = map PDP.dump . (PDP.initialMachine :) <$> PS.runSimulator' <$> PA.assemble str
+repl :: PDP.Machine -> String -> Either String [([Int], [Int], [Int], Int, PDP.ASM)]
+repl machine str = map PDP.dump <$> PS.runSimulator 64 machine <$> PA.assemble str
 
 asBits :: String -> Either String String
 asBits str = (unlines . map show . concatMap PDP.toBitBlocks) <$> PA.assemble str
+
+makeMachine :: Bool -> IO PDP.Machine
+makeMachine False = return PDP.initialMachine
+makeMachine True = do
+  i <- getStdRandom (randomR (0, 111))
+  j <- getStdRandom (randomR (0, 111))
+  k <- getStdRandom (randomR (0, 111))
+  return $ PDP.makePDP11 [0,0,mod i 256, div i 256, mod j 256, div j 256, mod k 256, div k 256, 1, 1] [0,0,0,0,0,0,0,100]
